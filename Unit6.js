818848995b8923adcdc2c2135d3af82a363c7554
YaroslavMyronov <ytube.noobtv@gmail.com>
2022-09-28 11:44:10 +0300

    Unit6

---
+++ Unit6.js
@@ -0,0 +1,8 @@
+var arr = [];                               //1
+arr[0] = 21;                                //2
+arr[1] = "Birthday";                        //3
+arr[2] = true;                              //4
+arr[3] = null;                              //5
+alert(arr);                                 //6
+arr.push(prompt("Введите свое значение:")); //7
+alert(arr);                                 //8
