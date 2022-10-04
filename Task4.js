0d7391baac87063eab7fd9b4e1ccecc00baf14a5
YaroslavMyronov <ytube.noobtv@gmail.com>
2022-10-04 18:45:12 +0300

    Two option

--- Task4.js
+++ Task4.js
@@ -1,10 +1,18 @@
-var n = 10;
-var factorial = 1;
-for (var i = 0;i<n;i++){
-    if (n===1 || n===0){
-        factorial = 1;
-    }else{
-        factorial = (n-i)*factorial;
-    }
+// var n = 10;
+// var factorial = 1;
+// for (var i = 0;i<n;i++){
+//     if (n===1 || n===0){
+//         factorial = 1;
+//     }else{
+//         factorial = (n-i)*factorial;
+//     }
+// }
+// console.log(factorial);
+
+function factorial(n) {
+    if (n<=0){
+       return 1;
+   }else {
+       return n * factorial(n-1);
+   }
 }
-console.log(factorial);
