fd269dbdc0750f18e852ad6f18c15929de823fd8
YaroslavMyronov <ytube.noobtv@gmail.com>
2022-10-25 13:19:44 +0300

    Unit20

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+function CreatePlayer(dictaphone, memorySize, size, wirelessConnection, workingHours, batteryCapacity, color) {
+    this.dictaphone = dictaphone;
+    this.memorySize = memorySize;
+    this.size = size;
+    this.wirelessConnection = wirelessConnection;
+    this.workingHours = workingHours;
+    this.batteryCapacity = batteryCapacity;
+    this.color = color;
+}
+
+var redPlayer = new CreatePlayer(false, 32, "109 х 72 х 16 мм", "Bluetooth 4.2", 19, 3200, "red");
+console.log(redPlayer);
